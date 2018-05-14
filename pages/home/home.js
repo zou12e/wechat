ee3a0f5a63da8d011686c66733c004eab5ca6d9e
zou12e <281933726@qq.com>
2018-05-15 01:39:59 +0800

    init

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,44 +1,85 @@
-// pages/home/home.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+// pages/square/square.js
+Page({
 
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        isMorning: true
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-      isMorning: true
-      
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        wx.setTabBarBadge({
+            index: 1,
+            text: '1'
+        })
+    },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
     toMorning(event) {
 
         this.setData({
             isMorning: true
         })
 
-      wx.setNavigationBarColor({
-        frontColor: '#ffffff',
-        backgroundColor: "#FFB307"
-      })
+        wx.setNavigationBarColor({
+            frontColor: '#ffffff',
+            backgroundColor: "#FFB307"
+        })
 
-      wx.setTabBarStyle({
-          backgroundColor: '#fff',
-      })
+        wx.setTabBarStyle({
+            backgroundColor: '#fff',
+        })
 
-      wx.setTabBarBadge({
-          index: 1,
-          text: '1'
-      })
+       
 
     },
     toNight(event) {
@@ -47,15 +88,15 @@ Component({
             isMorning: false
         })
 
-      wx.setNavigationBarColor({
-        frontColor: '#ffffff',
-        backgroundColor: "#00132A"
-      })
+        wx.setNavigationBarColor({
+            frontColor: '#ffffff',
+            backgroundColor: "#00132A"
+        })
 
-      wx.setTabBarStyle({
-          backgroundColor: '#fff',
-      })
+        wx.setTabBarStyle({
+            backgroundColor: '#fff',
+        })
     }
-  },
-  
 })
+ 
+ 
