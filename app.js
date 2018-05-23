32fb393718b4d0929ad34513bdc015b38f2ce308
zou12e <281933726@qq.com>
2018-05-23 22:30:03 +0800

    init

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
 App({
-  onLaunch: function () {
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          //发起网络请求
-          // wx.request({
-          //   url: `https://api.weixin.qq.com/sns/jscode2session?appid=wx037232cadf17facf&secret=8c2cd83e5243832f5067566e9d91a6a7&js_code=${res.code}&grant_type=authorization_code`,
-          // })
-        } else {
-          console.log('登录失败！' + res.errMsg)
+    onLaunch: function () {
+        wx.login({
+            success: (res) => {
+                if (res.code) {
+                    this.login(res.code);
+                } else {
+                    console.log('登录失败！' + res.errMsg)
+                }
+            }
+        });
+    },
+    globalData: {
+        userInfo: null,
+        host: 'http://192.168.4.248',
+        port: "4001",
+        apiversion: '/wechat/api/v1',
+    },
+    uri: function () {
+        return `${this.globalData.host}:${this.globalData.port + this.globalData.apiversion}`;
+    },
+    retain: (num) => {
+        num = "0" + parseInt(num);
+        return num.length > 2 ? num.substring(1) : num;
+    },
+    api: function (fn, data, type) {
+        const uri = this.uri(); 
+        const user = this.globalData.userInfo || {};
+        return new Promise( (resolve, reject) =>{
+            wx.request({
+                url: uri + fn,
+                method: type || 'GET',
+                header: {
+                    "user-sessionkey": user.session_key,
+                    "user-opeind": user.openid
+                },
+                data: data,
+                success: function (res) {
+                    resolve(res.data)
+                },
+                fail: function (res) {
+                    reject(res);
+                },
+            });
+        })
+    },
+    get: function (fn, data){
+        return this.api(fn, data);
+    },
+    post: function (fn, data) {
+        return this.api(fn, data,'POST');
+    },
+    login: async function (code) {
+        const ret = await this.post('/user/login', {
+            code: code
+        }).catch(function(err){
+            console.log(err);
+        });
+        if(ret && ret.code){
+            this.globalData.userInfo = ret.data;
+            if (this.userInfoReadyCallback) {
+                 this.userInfoReadyCallback()
+            }
         }
-      }
-    });
-  },
-  host: function () {
-    return "abc"
-  },
-  retain : function (num) {
-      num = "0" + parseInt(num);
-      return num.length > 2 ? num.substring(1) : num;
-  }
-
-
+    }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/square/square.js
+const app = getApp();
 Page({
 
     /**
@@ -6,34 +7,37 @@ Page({
      */
     data: {
         isMorning: true,
-        isPlay:false,
-        playInfo: {
-
-        }
+        isPlay: false,
+        playInfo: {},
+        data: {}
     },
-
     /**
-     * 生命周期函数--监听页面加载
+     * 获取早读晚讲信息
      */
-    onLoad: function (options) {
-        const playInfo =  {
-            title: "【月亮与六便士】",
-            src:"http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46",
-            time:401,
-            current:0
-        } ;
+    getReadSpeak: async function () {
+
+        const ret = await app.get('/home/getReadSpeak');
+        if (ret && ret.code === 1) {
+            this.setData({
+                data: ret.data
+            });
+            this.setPlayInfo();
+        }
+
+    },
+    setPlayInfo() {
+
+        const playInfo = this.data.data.read;
+        playInfo.current = 0;
         this.setData({
             playInfo: playInfo
         });
-        this.dialog = this.selectComponent("#dialog");
-        this.start = this.selectComponent("#startime");
-        this.end = this.selectComponent("#endtime");
 
         this.start.stom(0);
         this.end.stom(playInfo.time);
 
         this.innerAudioContext = wx.createInnerAudioContext();
-        this.innerAudioContext.src = playInfo.src;
+        this.innerAudioContext.src = playInfo.url;
         this.innerAudioContext.onPlay(() => {
             console.log('开始播放')
         })
@@ -58,11 +62,35 @@ Page({
             });
             this.start.stom(playInfo.current);
         })
+    },
+    /**
+     * 获取用户信息
+     */
+    getUserInfo: function () {
+        if (app.globalData.userInfo) {
+            this.getReadSpeak();
+        } else {
+            app.userInfoReadyCallback = () => {
+                this.getReadSpeak();
+            };
+        }
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: async function (options) {
 
-        wx.setTabBarBadge({
-            index: 1,
-            text: '1'
-        })
+
+        this.dialog = this.selectComponent("#dialog");
+        this.start = this.selectComponent("#startime");
+        this.end = this.selectComponent("#endtime");
+
+        this.getUserInfo();
+
+        // wx.setTabBarBadge({
+        //     index: 2,
+        //     text: '1'
+        // })
     },
 
     /**
@@ -74,8 +102,8 @@ Page({
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
-        
+    onShow: async function () {
+
     },
     /**
      * 生命周期函数--监听页面隐藏
@@ -110,26 +138,24 @@ Page({
     onShareAppMessage: function () {
 
     },
-    play: function(){
+    play: function () {
 
         this.setData({
             isPlay: !this.data.isPlay
         })
 
-        if (this.data.isPlay){
+        if (this.data.isPlay) {
             this.innerAudioContext.play();
-        }else {
+        } else {
             this.innerAudioContext.pause();
         }
 
     },
-    stop:function(){
+    stop: function () {
         this.setData({
             isPlay: false
         })
-
         this.innerAudioContext.pause();
-      
     }
     ,
     toMorning(event) {
@@ -147,7 +173,7 @@ Page({
             backgroundColor: '#fff',
         })
 
-       
+
 
     },
     toNight(event) {
@@ -165,27 +191,30 @@ Page({
             backgroundColor: '#fff',
         })
     },
-    toUrl:function(){
-        
-        if (this.data.isMorning){
+    toUrl: function () {
+
+        if (this.data.isMorning) {
             wx.navigateTo({
                 url: '/pages/home/read/read'
             })
-        }else {
+        } else {
             wx.navigateTo({
                 url: '/pages/home/speak/speak'
             })
         }
     },
-    showContent:function(){
+    /**
+     * 查看文章内容
+     */
+    showContent: function () {
 
         this.dialog.togglerShow({
-            title:"【月亮与六便士】",
-            author:"[英]威廉·萨默塞特·毛姆",
-            content: "世界上只有少数人能够最终达到自己的理想。我们的生活很单纯、很简朴。我们并不野心勃勃，如果说我们也有骄傲的话，那是因为在想到通过双手获得的劳动成果时的骄傲。我们对别人既不嫉妒，更不怀恨。唉，我亲爱的先生，有人认为劳动的幸福是句空话，对我说来可不是这样。我深深感到这句话的重要意义。我是个很幸福的人。世界上只有少数人能够最终达到自己的理想。我们的生活很单纯、很简朴。我们并不野心勃勃，如果说我们也有骄傲的话，那是因为在想到通过双手获得的劳动成果时的骄傲。我们对别人既不嫉妒，更不怀恨。唉，我亲爱的先生，有人认为劳动的幸福是句空话，对我说来可不是这样。我深深感到这句话的重要意义。我是个很幸福的人。"
+            title: this.data.data.read.title,
+            author: this.data.data.read.author,
+            content: this.data.data.read.content
         });
     },
-    changeTime: function(event){
+    changeTime: function (event) {
 
         var playInfo = this.data.playInfo;
         playInfo.current = (event && event.detail.value) || 0;
@@ -195,20 +224,34 @@ Page({
         this.start.stom(playInfo.current);
         this.innerAudioContext.seek(playInfo.current);
     },
-    changeAudio: function(){
+    changeAudio: async function () {
+        const ret = await app.get('/home/changeInfo', { type: this.data.isMorning ? 1 : 2 });
+        if (ret && ret.code === 1) {
+            const data = this.data.data;
+            if (this.data.isMorning) {
+                data.read = ret.data;
+                this.stop();
+                this.setPlayInfo();
+            } else {
+                data.speak = ret.data;
+            }
+            this.setData({
+                data: data
+            });
+           
+        }
 
-        const playInfo = {
-            title:"【追风筝的人】",
-            src: "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46",
-            time: 401,
-            current: 0
-        };
-        this.setData({
-            playInfo: playInfo
-        });
-        this.innerAudioContext.seek(playInfo.current);
-        this.stop();
+        // const playInfo = {
+        //     title: "【追风筝的人】",
+        //     url: "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46",
+        //     time: 401,
+        //     current: 0
+        // };
+        // this.setData({
+        //     playInfo: playInfo
+        // });
+        // this.innerAudioContext.seek(playInfo.current);
+        // this.stop();
     }
 })
- 
- 
+
